32e2e847dbb2d939b03508bc6282f9275c03526f
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-18 16:37:02 +0300

    commit tree c2b1e552bafc18d40f041bac2cce242ae95a8589

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+    // const input = document.getElementById('input');
+    // input.addEventListener('click', function() {
+    //   alert('I was clicked!');
+    // });
+
+// const input = document.querySelector('input');
+
+// function clickAlert() {
+//   input.addEventListener('click', clickAlert);
+//  return  alert('I was clicked!');
+// }
+
+const input = document.querySelector('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
+
+
